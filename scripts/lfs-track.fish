6ef84e408b0bfa4e4a12a47389542c780eb05b0e
Aldrin <DrinMontana@acm.org>
2020-05-12 13:56:48 -0700

    Updated lfs tracking and supporting files

--- scripts/lfs-track.fish
+++ scripts/lfs-track.fish
@@ -1,11 +1,11 @@
 # broadly determine image files to track
-find assets/ -name '*.psd' -type f -printf '%f\n' > resources/track.lfs.txt
-find assets/ -name '*.eps' -type f -printf '%f\n' >> resources/track.lfs.txt
-find assets/ -name '*.jpg' -type f -printf '%f\n' >> resources/track.lfs.txt
-find assets/ -name '*.png' -type f -printf '%f\n' >> resources/track.lfs.txt
+find assets/ -name '*.psd' -type f >> resources/track.lfs.txt
+find assets/ -name '*.eps' -type f >> resources/track.lfs.txt
+find assets/ -name '*.jpg' -type f >> resources/track.lfs.txt
+find assets/ -name '*.png' -type f >> resources/track.lfs.txt
 
 # determine files that are in only track.lfs.txt
-comm -3 -1 resources/no-track.lfs.txt resources/track.lfs.txt
+#comm -3 -1 resources/no-track.lfs.txt resources/track.lfs.txt
 
 # LFS track only the files in track.lfs.txt
-git lfs track (comm -3 -1 resources/no-track.lfs.txt resources/track.lfs.txt)
+#git lfs track (comm -3 -1 resources/no-track.lfs.txt resources/track.lfs.txt)
